ae5579d1773e1746a10d2c1e5129e8fcb4bcbe3b
Dillan Teagle <dillan.teagle@vertikalrms.com>
2022-01-16 10:54:33 -0600

    prepare for new role for aws

--- run.sh
+++ run.sh
@@ -3,7 +3,12 @@ set -euo pipefail
 
 playbook() {
   echo "running playbook ${1}"
-  ansible-playbook -i inventory.ini playbooks/${1}.yaml
+  envsubst | ansible-playbook -i inventory/hosts.ini \
+    --ask-vault-pass  \
+    --extra-vars \
+    "ansible_user=${ANSIBLE_USER} \
+    ansible_password=${ANSIBLE_PASSWORD}" \
+    playbooks/${1}.yaml
 }
 
 check_temp() {
